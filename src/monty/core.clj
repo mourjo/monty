70e74fc552684d522640d5ad6515c1628b3d4b80
Mourjo Sen <hello@mourjo.me>
2022-03-20 18:22:55 +0530

    Add more graphs

--- src/monty/core.clj
+++ src/monty/core.clj
@@ -2,70 +2,86 @@
   (:require [oz.core :as oz])
   (:gen-class))
 
+
+(defn door-selections
+  [n-doors n-removals]
+  {:pre [(< n-removals (dec n-doors))]}
+  (let [all-doors (range n-doors)
+        winning-door (rand-int n-doors)
+        contestant-first-selection (rand-int n-doors)
+        host-selections (->> all-doors
+                             shuffle
+                             (remove (set [winning-door contestant-first-selection]))
+                             (take n-removals)
+                             set)
+        chosen-already? (conj host-selections contestant-first-selection)
+        contestant-switched-selection (first (remove chosen-already? (shuffle all-doors)))]
+    {:first-selection contestant-first-selection
+     :winning-door winning-door
+     :host-selections host-selections
+     :switched-selection contestant-switched-selection}))
+
+
 (defn experiment
-  [n]
-  (let [winning-door (rand-int n)
-        contestant-first-selection (rand-int n)
-        host-selection (->> (repeatedly #(rand-int n))
-                            (remove (set [contestant-first-selection winning-door]))
-                            first)
-        contestant-switched-selection (->> (repeatedly #(rand-int n))
-                                           (remove (set [host-selection contestant-first-selection]))
-                                           first)]
-    [(if (= winning-door contestant-first-selection) 1 0)
-     (if (= winning-door contestant-switched-selection) 1 0)]))
+  [n-doors n-removals]
+  (let [{:keys [first-selection switched-selection winning-door]}
+        (door-selections n-doors n-removals)]
+    [(if (= winning-door first-selection) 1 0)
+     (if (= winning-door switched-selection) 1 0)]))
 
 
 (defn repeated-experiment
-  [n-experiments n-doors]
+  [n-experiments n-doors n-removals]
   (let [[first-selection-wins
          second-selection-wins] (reduce (fn [[prev-a prev-b] [a b]]
                                           [(+ a prev-a) (+ b prev-b)])
                                         [0 0]
-                                        (repeatedly n-experiments #(experiment n-doors)))] 
+                                        (repeatedly n-experiments #(experiment n-doors n-removals)))] 
     [(* 100 (double (/ first-selection-wins n-experiments)))
      (* 100 (double (/ second-selection-wins n-experiments)))]))
 
 
 (defn monte-carlo
-  [n-experiments n-doors]
-  (mapcat (fn [num-experiment]
-            (let [[no-switch-wins switch-wins] (repeated-experiment num-experiment n-doors)]
-              [{:num-experiments num-experiment :type :no-switch :win-percent no-switch-wins}
-               {:num-experiments num-experiment :type :switch :win-percent switch-wins}]))
-          (range 1 (inc n-experiments))))
+  [n-experiments n-doors n-removals & {:keys [width height]
+                                       :or {width 200 height 200}}]
+  (let [data (mapcat (fn [num-experiment]
+                       (let [[no-switch-wins switch-wins] (repeated-experiment num-experiment n-doors n-removals)]
+                         [{:num-experiments num-experiment :type :no-switch :win-percent no-switch-wins}
+                          {:num-experiments num-experiment :type :switch :win-percent switch-wins}]))
+                     (range 1 (inc n-experiments)))]
+    {:data {:values data}
+     :width width
+     :height height
+     :title (format "%d experiments, %d doors, %d removals" n-experiments n-doors n-removals)
+     :encoding {:x {:field "num-experiments" :type "quantitative"}
+                :y {:field "win-percent" :type "quantitative"}
+                :color {:field "type" :type "nominal"}}
+     :mark "line"}))
+
+
+(defn render!
+  []
+  (oz/view!
+   [:div
+    [:div {:style {:display "flex" :flex-direction "row"}}
+     [:vega-lite (monte-carlo 150 3 1)]
+     [:vega-lite (monte-carlo 2000 3 1)]
+     [:vega-lite (monte-carlo 2000 5 1)]]
+
+    [:hr]
+    
+    [:div {:style {:display "flex" :flex-direction "row"}}
+     [:vega-lite (monte-carlo 500 100 70)]
+     [:vega-lite (monte-carlo 500 100 80)]
+     [:vega-lite (monte-carlo 500 100 90)]]
+
+    [:div {:style {:display "flex" :flex-direction "row"}}
+     [:vega-lite (monte-carlo 500 100 96)]
+     [:vega-lite (monte-carlo 500 100 97)]
+     [:vega-lite (monte-carlo 500 100 98)]]]))
 
 
 (defn -main
   [& args]
-  (let [expt-150-3 {:data {:values (monte-carlo 150 3)}
-                    :width 600
-                    :height 600
-                    :encoding {:x {:field "num-experiments" :type "quantitative"}
-                               :y {:field "win-percent" :type "quantitative"}
-                               :color {:field "type" :type "nominal"}}
-                    :mark "line"}
-
-        expt-2000-3 {:data {:values (monte-carlo 2000 3)}
-                     :width 600
-                     :height 600
-                     :encoding {:x {:field "num-experiments" :type "quantitative"}
-                                :y {:field "win-percent" :type "quantitative"}
-                                :color {:field "type" :type "nominal"}}
-                     :mark "line"}
-
-        expt-2000-5 {:data {:values (monte-carlo 2000 5)}
-                     :width 600
-                     :height 600
-                     :encoding {:x {:field "num-experiments" :type "quantitative"}
-                                :y {:field "win-percent" :type "quantitative"}
-                                :color {:field "type" :type "nominal"}}
-                     :mark "line"}]
-    (oz/view!
-     [:div
-      [:h1 "150 Experiments with 3 Doors"]
-      [:vega-lite expt-150-3]
-      [:h1 "2000 Experiments with 3 Doors"]
-      [:vega-lite expt-2000-3]
-      [:h1 "2000 Experiments with 5 Doors"]
-      [:vega-lite expt-2000-5]])))
+  (oz/start-server!)
+  (render!))
---
+++ test/monty/core_test.clj
@@ -0,0 +1,27 @@
+(ns monty.core-test
+  (:require [monty.core :as sut]
+            [clojure.test :as t]))
+
+
+
+(t/deftest door-selections-test
+  (doseq [_ (range 20)
+          [n-doors n-removals] [[3 1]
+                                [5 1]
+                                [5 2]
+                                [5 3]
+                                [10 8]
+                                [100 80]]]
+    (let [{:keys [first-selection switched-selection host-selections]}
+          (sut/door-selections n-doors n-removals)]
+      (t/is (not (host-selections first-selection))
+            "Host should not select a contestant's first selection")
+      (t/is (not (host-selections switched-selection))
+            "Contestant should not select host's selection as their second selection"))))
+
+
+
+(t/deftest expected-probability-of-win
+  (let [[no-switch-win-percent switch-win-percent] (sut/repeated-experiment 1000000 3 1)]
+    (t/is (= 33.0 (Math/floor no-switch-win-percent)))
+    (t/is (= 66.0 (Math/floor switch-win-percent)))))
