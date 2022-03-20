dd23dce9db73b32f8f530b45496c4b22c4c0e5e2
Mourjo Sen <hello@mourjo.me>
2022-03-20 18:34:05 +0530

    Do not import graphing dependencies in test

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [metasoarous/oz "2.0.0-alpha5"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[metasoarous/oz "2.0.0-alpha5"]]}
+             :provided {:dependencies [[metasoarous/oz "2.0.0-alpha5"]]}
+             :test {:dependencies []}
+             :uberjar {:aot :all
+                       :dependencies [[metasoarous/oz "2.0.0-alpha5"]]}}
   :main monty.core
   :repl-options {:init-ns monty.core})
