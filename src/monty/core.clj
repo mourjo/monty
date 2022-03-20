40956ff270836a12731182af4ceaed92cddd639e
Mourjo Sen <hello@mourjo.me>
2022-03-20 21:38:26 +0530

    Add heatmap

--- src/monty/core.clj
+++ src/monty/core.clj
@@ -8,13 +8,15 @@
   []
   (oz/view!
    [:div
+    [:h3 "How does win percent change with number of doors?"]
     [:div {:style {:display "flex" :flex-direction "row"}}
      [:vega-lite (me/monte-carlo-plot 500 3 1)]
      [:vega-lite (me/monte-carlo-plot 500 5 1)]
      [:vega-lite (me/monte-carlo-plot 500 10 1)]]
 
     [:hr]
-    
+
+    [:h3 "What if there are 100 doors and many are revealed?"]
     [:div {:style {:display "flex" :flex-direction "row"}}
      [:vega-lite (me/monte-carlo-plot 500 100 70)]
      [:vega-lite (me/monte-carlo-plot 500 100 80)]
@@ -23,7 +25,14 @@
     [:div {:style {:display "flex" :flex-direction "row"}}
      [:vega-lite (me/monte-carlo-plot 500 100 96)]
      [:vega-lite (me/monte-carlo-plot 500 100 97)]
-     [:vega-lite (me/monte-carlo-plot 500 100 98)]]]))
+     [:vega-lite (me/monte-carlo-plot 500 100 98)]]
+
+    [:hr]
+
+    [:h3 "How does win percent change with reveals?"]
+    [:div {:style {:display "flex" :flex-direction "row"}}
+     [:vega-lite (me/heat-map-plot 50 100 false)]
+     [:vega-lite (me/heat-map-plot 50 100 true)]]]))
 
 
 (defn -main
--- src/monty/experiment.clj
+++ src/monty/experiment.clj
@@ -1,15 +1,15 @@
 (ns monty.experiment)
 
 (defn door-selections
-  [doors removals]
-  {:pre [(< removals (dec doors))]}
+  [doors reveals]
+  {:pre [(< reveals (dec doors))]}
   (let [all-doors (range doors)
         winning-door (rand-int doors)
         contestant-first-selection (rand-int doors)
         host-selections (->> all-doors
                              shuffle
                              (remove (set [winning-door contestant-first-selection]))
-                             (take removals)
+                             (take reveals)
                              set)
         chosen-already? (conj host-selections contestant-first-selection)
         contestant-switched-selection (->> all-doors
@@ -23,37 +23,62 @@
 
 
 (defn run-experiment
-  [doors removals]
+  [doors reveals]
   (let [{:keys [first-selection switched-selection winning-door]}
-        (door-selections doors removals)]
+        (door-selections doors reveals)]
     [(if (= winning-door first-selection) 1 0)
      (if (= winning-door switched-selection) 1 0)]))
 
 
 (defn compute-win-percent
-  [experiments doors removals]
+  [experiments doors reveals]
   (let [[first-selection-wins
          second-selection-wins] (reduce (fn [[prev-a prev-b] [a b]]
                                           [(+ a prev-a) (+ b prev-b)])
                                         [0 0]
-                                        (repeatedly experiments #(run-experiment doors removals)))] 
+                                        (repeatedly experiments #(run-experiment doors reveals)))] 
     [(* 100 (double (/ first-selection-wins experiments)))
      (* 100 (double (/ second-selection-wins experiments)))]))
 
 
 (defn monte-carlo-plot
-  [experiments doors removals & {:keys [width height]
+  [experiments doors reveals & {:keys [width height]
                                  :or {width 200 height 200}}]
   (let [data (mapcat (fn [num-experiment]
-                       (let [[no-switch-wins switch-wins] (compute-win-percent num-experiment doors removals)]
+                       (let [[no-switch-wins switch-wins] (compute-win-percent num-experiment doors reveals)]
                          [{:num-experiments num-experiment :type :no-switch :win-percent no-switch-wins}
                           {:num-experiments num-experiment :type :switch :win-percent switch-wins}]))
                      (range 1 (inc experiments)))]
     {:data {:values data}
      :width width
      :height height
-     :title (format "%d experiments, %d doors, %d removals" experiments doors removals)
+     :title (format "%d experiments, %d doors, %d reveals" experiments doors reveals)
      :encoding {:x {:field "num-experiments" :type "quantitative"}
                 :y {:field "win-percent" :type "quantitative"}
                 :color {:field "type" :type "nominal"}}
      :mark "line"}))
+
+
+(defn heat-map-plot
+  [experiments max-doors switch? & {:keys [width height]
+                                    :or {width 400 height 400}}]
+  {:pre [(<= 3 max-doors)]}
+  (let [data (for [doors (range 3 (inc max-doors))
+                   reveals (range 1 (inc (- doors 2)))]
+               (let [[first-selection-percent second-selection-percent] (compute-win-percent experiments doors reveals)]
+                 {:win-percent (if switch? second-selection-percent first-selection-percent)
+                  :number-of-doors doors
+                  :number-of-reveals-by-host reveals}))]
+    {:$schema "https://vega.github.io/schema/vega-lite/v5.json",
+     :data {:values data},
+     :mark "rect",
+     :title (format "%d experiments, %s" experiments (if switch? "when switching" "when not switching"))
+     :width width,
+     :height height,
+     :encoding
+     {:x {:bin {:maxbins max-doors}, :field "number-of-doors", :type "quantitative"},
+      :y {:bin {:maxbins max-doors}, :field "number-of-reveals-by-host", :type "quantitative"},
+      :color {:aggregate "avg", :field "win-percent" :type "quantitative"}},
+     :config {:view {:stroke "transparent"}}}))
+
+
